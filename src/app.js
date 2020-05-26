4b7e938fd2bfaf303cb952fde983f4f4a63799a7
Sam <sam.krasnoff@gmail.com>
2020-05-26 13:29:23 -0400

    Initial commit

---
+++ src/app.js
@@ -0,0 +1,92 @@
+const path = require('path')
+const express = require('express')
+const hbs = require('hbs')
+const geocode = require('./utils/geocode')
+const forecast = require('./utils/forecast')
+
+
+const app = express();
+const publicDirectoryPath = path.join(__dirname, '../public')
+const partialsPath = path.join(__dirname, 'templates/partials')
+app.set('view engine','hbs')
+app.set('views', path.join(__dirname, 'templates/views'));
+app.use(express.static('public'))
+hbs.registerPartials(partialsPath)
+
+app.get('',(req, res) => {
+    res.render('index',{
+        title: 'Weather App',
+        name: 'Sam'
+    })
+})
+app.get('/about',(req,res) => {
+    res.render('about',{
+        title: 'About Me',
+        name: 'Sam'
+    })
+})
+app.get('/weather', (req, res) => {
+    if(!req.query.address){
+        return res.send({
+            error: "Address required"
+        })
+    }
+    geocode(req.query.address, (error, { latitude, longitude, location } = {}) => {
+        if (error) {
+            return res.send({
+                error: "Error Occured"
+            })
+        }
+        forecast(latitude, longitude, (error, forecastData) => {
+            if (error) {
+                return res.send({
+                    error: "Error Occured"
+                })
+            }
+            res.send({
+                forecast: forecastData,
+                location: location,
+                address: req.query.address
+            })
+        })
+    })
+})
+
+app.get('/products', () => {
+    if(!req.query.search){
+        return res.send({
+            error: "Search term required"
+        })
+    }
+    res.send({
+        products: []
+    })
+})
+
+app.get('/help', (req,res) => {
+    res.render('help',{
+        title: 'Help',
+        helper: 'This is a help message for anyone that needs to see it.',
+        name: 'SAMUEL'
+    })
+})
+
+app.get('/help/*', (req, res) => {
+    res.render('catchall',{
+        title: 'Error 404',
+        name: 'Sam',
+        error: 'Help article not found'
+    })
+})
+
+app.get('*', (req, res) => {
+    res.render('catchall',{
+        title: 'Error 404',
+        name: 'Sam',
+        error: 'Page not found'
+    })
+})
+
+app.listen(3000, () => {
+    console.log('Server is up on port 3000.')
+})
