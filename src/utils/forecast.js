a9e845996e13b899a0636070d8a4cbe85f100646
Sam <sam.krasnoff@gmail.com>
2020-05-26 21:23:30 -0400

    changed location API, added some other info

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -1,15 +1,16 @@
 const request = require('request')
 
 const forecast = (latitude, longitude, callback) => {
-    const url = 'https://api.darksky.net/forecast/bf9f46a339e557c9d25d6ed0b259a45f/' + latitude + ',' + longitude
-
+    //const url = 'https://api.darksky.net/forecast/bf9f46a339e557c9d25d6ed0b259a45f/' + latitude + ',' + longitude
+    const url = 'http://api.weatherstack.com/current?access_key=7465f4e7c4eeb8e8ed5bc1c688efa6c4&units=f&query=' + latitude + ',' + longitude
     request({ url, json: true }, (error, { body }) => {
         if (error) {
             callback('Unable to connect to weather service!', undefined)
         } else if (body.error) {
             callback('Unable to find location', undefined)
         } else {
-            callback(undefined, body.daily.data[0].summary + ' It is currently ' + body.currently.temperature + ' degress out. There is a ' + body.currently.precipProbability + '% chance of rain.')
+            callback(undefined, body.current.weather_descriptions[0] + '. It is currently ' + body.current.temperature + ' degress out. It feels like ' + body.current.feelslike + ' degrees out. The humidity is ' + body.current.humidity + '%.')
+            //callback(undefined, body.daily.data[0].summary + ' It is currently ' + body.currently.temperature + ' degress out. The high today is ' + body.daily.data[0].temperatureHigh + ' with a low of' + body.daily.data[0].temperatureLow + '. There is a ' + body.currently.precipProbability + '% chance of rain.')
         }
     })
 }
